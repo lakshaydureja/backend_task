72b26e7a6f858c806df73408aa50420b8ec1b81c
Lakshay Dureja <lakshaydureja7@gmail.com>
2024-12-03 01:02:56 +0530

    updated the port

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from app.routes.students import router as students_router
 
-app = FastAPI(title="Student collection API")
-
+app = FastAPI(title="Backend Intern Hiring Task")
 app.include_router(students_router)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
